e3c3104d6b92465407250b7a2ce81e5c01147995
Sh1kataGaNai <g.w.hitecode@gmail.com>
2017-12-16 22:48:21 -0500

    Lab3 Complete

---
+++ data_module.js
@@ -0,0 +1,236 @@
+module.exports = {
+   getDataNews: function() {
+   	  return {
+   	  	"data_news": {
+		"1": {
+			"image": "images/news/persistance.png",
+			"time": "Опубликовано 12-12-2017",
+			"description": "This post is part of the Empire Series with some background and an ongoing list of series posts [kept here]. Code execution is great and remote control is awesome, but if you don’t have a persistence strategy planned nothing can throw a wrench in your engagement like an unplanned reboot or user logout. This post covers 17 current Empire persistence modules that can help you with retaining hard-fought access, broken into userland/elevated options, PowerBreach, and miscellaneous approaches.We like to break reboot persistence down into a three different questions. First, are you installing the persistence from userland or an elevated context? Second, where are you storing the payload you want to execute? And finally, how do you want the system to trigger your malicious logic?",
+			"link": "http://www.harmj0y.net/blog/empire/nothing-lasts-forever-persistence-with-empire/",
+			"alt": "Empire persistence"
+		},
+		"2": {
+			"image": "images/news/exceldde.png",
+			"time": "Опубликовано 12-12-2017",
+			"description": "Before I get into these techniques, it’s important to understand the normal behavior. Attackers often use a number of tricks to get code-execution on a target system. This often ranges from Office macros, to OLE objects and Excel formula injection via DDE. If we embed a LNK into an Excel document via OLE, we will see this locally...",
+			"link": "https://enigma0x3.net/2017/07/13/phishing-against-protected-view/",
+			"alt": "excel dde attack"
+		},
+		"3": {
+			"image": "images/news/mail.jpg",
+			"time": "Опубликовано 13-12-2017",
+			"description": "Как сообщают «Ведомости» со ссылкой на представителя проекта, вице-президент Mail.Ru Group Владимир Никольский намерен запустить сервис удаленных консультаций с врачами Doctor Smart, в котором будут задействованы технологии блокчейна и искусственного интеллекта. По планам Никольского,сервис должен заработать в начале 2018 года.Пользователям сервиса будут доступны онлайн - консультации терапевтов, диетологов, ветеринаров и других специалистов.В дальнейшем будет добавлена возможность записаться в клинику или лабораторию для анализов.Проект будет сотрудничать с клиниками, фитнес - центрами и врачами, которые имеют дипломы, действующую сертификацию и пройдут тестирование Doctor Smart.",
+			"link": "https://bits.media/news/vitse-prezident-mail-ru-zapuskaet-udalennye-konsultatsii-s-vrachami/",
+			"alt": "Mail.ru feature"
+		},
+		"4": {
+			"image": "images/news/cryptorbtc.jpg",
+			"time": "Опубликовано 13-12-2017",
+			"description": "Кибератаки, которые шифруют данные и предлагают дешифровать их за выкуп, выплачиваемый в биткоинах - уже не новость. За последние пару лет было несколько массовых атак этого типа. Самые последние из них, получившие наиболее широкое распространение и атаковавшие компьютеры по всему миру – Petya, WannaCry и Bad Rabbit. Однако мошенники все чаще сталкиваются с проблемой – никто не хочет платить выкуп.",
+			"link": "https://bits.media/news/problemy-khakerov-nikto-ne-khochet-platit-vykup-v-bitkoinakh/",
+			"alt": "BTC ransomware"
+		},
+		"5": {
+			"image": "images/news/coinhive.jpg",
+			"time": "Опубликовано 13-12-2017",
+			"description": "В середине сентября 2017 года владельцы торрент-трекера The Pirate Bay начали экспериментировать с новым способом монетизации: когда пользователь заходит на сайт, специальный скрипт начинает майнить криптовалюту прямо в его браузере. Аналогичный способ заработка используют и мошенники, причем их действия направлены прежде всего на русскоязычных пользователей.",
+			"link": "https://meduza.io/feature/2017/09/18/nekotorye-sayty-zastavlyayut-vash-brauzer-maynit-kriptovalyutu-kak-eto",
+			"alt": "coin-hive.io"
+		}
+	}
+	}
+	},
+	getDataGallery: function(){
+		return {
+		"data_gallery": {
+		"1": {
+			"image": "http://i0.kym-cdn.com/photos/images/newsfeed/000/096/044/trollface.jpg?1296494117",
+			"description": "Trollface1",
+			"alt": "troll1"
+		},
+		"2": {
+			"image": "http://vignette4.wikia.nocookie.net/warframe/images/5/5f/Famous-characters-Troll-face-45719.png/revision/latest?cb=20140702154027",
+			"description": "Trollface2",
+			"alt": "troll2"
+		},
+		"3": {
+			"image": "https://i.pinimg.com/236x/a2/4e/be/a24ebe581b38bb2a07664dc4b8af7f25--sienna-troll-face.jpg",
+			"description": "Trollface3",
+			"alt": "troll3"
+		},
+		"4": {
+			"image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9ycYuR3WoRCjyv-hL2z5dLWb-1QhOKqvCJFI0LvTTS63ijyBZ",
+			"description": "Trollface4",
+			"alt": "troll4"
+		},
+		"5": {
+			"image": "http://i0.kym-cdn.com/photos/images/facebook/000/103/740/c2c.png",
+			"description": "Trollface5",
+			"alt": "troll5"
+		},
+		"6": {
+			"image": "https://2.bp.blogspot.com/-DogEf13UX9A/UjYMttf5mSI/AAAAAAAABD8/ei2oZ-woZOY/s1600/Famous-characters-Troll-face-Troll-face-me-gusta-51996.png",
+			"description": "Trollface6",
+			"alt": "troll6"
+		},
+		"7": {
+			"image": "https://vignette.wikia.nocookie.net/prodigy-math-game/images/8/87/Trollface_%282%29.jpg/revision/latest?cb=20171113052318",
+			"description": "Trollface7",
+			"alt": "troll7"
+		},
+		"8": {
+			"image": "https://lh3.googleusercontent.com/160fZWxIusL8JGRwL43bzH4ccHZ9ViIMyFSLhFEMACa_3t8VdPzcwFSZKiGCwoPxqLA=w300",
+			"description": "Trollface8",
+			"alt": "troll8"
+		}
+	}
+}
+
+	
+	},
+	getDataInterests: function(){
+		return {
+			"data_interests": {
+		"1": {
+			"image": "images/index/empire.png",
+			"description": "Empire is a pure PowerShell post-exploitation agent built on cryptologically-secure communications and a flexible architecture.Empire implements the ability to run PowerShell agents without needing powershell.exe, rapidly deployable post - exploitation modules ranging from key loggers to Mimikatz, and adaptable communications to evade network detection, all wrapped up in a usability - focused framework.It premiered at BSidesLV in 2015. ",
+			"alt": "Empire"
+		},
+		"2": {
+			"image": "images/index/metasploit.png",
+			"description": "The Metasploit Project is a computer security project that provides information about security vulnerabilities and aids in penetration testing and IDS signature development. Its best - known sub - project is the open source Metasploit Framework, a tool for developing and executing exploit code against a remote target machine.Other important sub - projects include the Opcode Database, shellcode archive and related research.The Metasploit Project is well knownfor its anti - forensic and evasion tools,some of which are built into the Metasploit Framework.",
+			"alt": "Metasploit"
+		},
+		"3": {
+			"image": "images/index/django.png",
+			"description": "Django is a high-level Python Web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of Web development, so you can focus on writing your app without needing to reinvent the wheel. It's free and open source.",
+			"alt": "Django"
+		},
+		"4": {
+			"image": "images/index/angular.png",
+			"description": "AngularJS is what HTML would have been, had it been designed for building web-apps. Declarative templates with data-binding, MVW, MVVM, MVC, dependency injection and great testability story all implemented with pure client-side JavaScript!",
+			"alt": "AngularJS"
+		}
+
+		}
+	
+		}
+	},
+	getDataShedule: function(){
+		return {
+			"data_shedule": {
+		"lesson_1": {
+			"2": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"3": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"4": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"5": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"6": {
+				"subject": "Теорія ймовірностей та мат. статистика",
+				"teacher": "Левчук  305(6)"
+			}
+		},
+		"lesson_2": {
+			"2": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"3": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"4": {
+				"subject": "Теорія ймовірностей та мат. статистика",
+				"teacher": "Левчук  62(1)"
+			},
+			"5": {
+				"subject": "Осн. програм. інженерії",
+				"teacher": "Кривохата  213(ск)"
+			},
+			"6": {
+				"subject": "Конструювання програмного забезпечення",
+				"teacher": "Мильцев  62(1)"
+			}
+		},
+		"lesson_3": {
+			"2": {
+				"subject": "Front-End технології (лекція)",
+				"teacher": "Лісняк  54(1)"
+			},
+			"3": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"4": {
+				"subject": "Практикум з програмування",
+				"teacher": "Ткаченко  305(6)"
+			},
+			"5": {
+				"subject": "Осн. програм. інженерії",
+				"teacher": "Кривохата 104(2)"
+			},
+			"6": {
+				"subject": "Конструювання програмного забезпечення",
+				"teacher": "Швидка  213(6)"
+			}
+		},
+		"lesson_4": {
+			"2": {
+				"subject": "Front-End технології (2 гр.)",
+				"teacher": "Лісняк  54(1)"
+			},
+			"3": {
+				"subject": "Практикум з програмування",
+				"teacher": "Ткаченко 40(1)"
+			},
+			"4": {
+				"subject": "Осн. програм. інженерії",
+				"teacher": "Чопоров 62(1)"
+			},
+			"5": {
+				"subject": "Іноземна мова",
+				"teacher": "Медвєдєва 310(6)"
+			},
+			"6": {
+				"subject": "-",
+				"teacher": "-"
+			}
+		},
+		"lesson_5": {
+			"2": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"3": {
+				"subject": "Практикум з програмування",
+				"teacher": "Ткаченко 40(1)"
+			},
+			"4": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"5": {
+				"subject": "-",
+				"teacher": "-"
+			},
+			"6": {
+				"subject": "-",
+				"teacher": "-"
+			}
+		}
+
+ }
+		}
+	}
+}
---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var autoPrefixer = require('gulp-autoprefixer');
+//if node version is lower than v.0.1.2
+require('es6-promise').polyfill();
+var cssComb = require('gulp-csscomb');
+var cmq = require('gulp-merge-media-queries');
+var cleanCss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var jade = require('gulp-jade');
+var data = require('gulp-data');
+var fs = require('fs');
+var data_module = require('./data_module');
+
+
+
+gulp.task('css',function(){
+    gulp.src(['css/src/**/*.css'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(autoPrefixer())
+        .pipe(cssComb())
+        .pipe(cmq({log:true}))
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('css/dist'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('css/dist'))
+});
+gulp.task('js',function(){
+    gulp.src(['js/src/**/*.js'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(gulp.dest('js/dist'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('js/dist'))
+});
+gulp.task('jade',function(){
+    gulp.src(['html/pages/*.jade'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(data(function(file) {
+                  var data_news = JSON.parse(JSON.stringify(data_module.getDataNews()));
+                  var data_gallery = JSON.parse(JSON.stringify(data_module.getDataGallery()));
+                  var data_interests = JSON.parse(JSON.stringify(data_module.getDataInterests()));
+                  var data_shedule = JSON.parse(JSON.stringify(data_module.getDataShedule()));
+                  var data = {"news": data_news, "gallery": data_gallery, "interests": data_interests, "shedule": data_shedule
+              };
+                
+                  //console.log(data);
+                  /*
+                  for (var i = 1; i < Object.keys(data['news']['data_news']).length + 1; i++)
+                  console.log(data['news']['data_news'][i.toString()]['image']);
+                  */
+                  return data;
+
+                       
+                       
+
+                } )) 
+        .pipe(jade({
+
+        }))
+        .pipe(gulp.dest('./'))
+});
+gulp.task('default',function(){
+    gulp.watch('js/src/**/*.js',['js']);
+    gulp.watch('css/src/**/*.css',['css']);
+    gulp.watch('html/**/*.jade',['jade']);
+});
